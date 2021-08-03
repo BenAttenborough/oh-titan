74ccc6a9a06eb401328bdfc800251e0aeaef1091
Ben Attenborough <battenborough@gmail.com>
2021-08-03 20:16:04 +0100

    Modify the instructions

--- states/InstructionsState.lua
+++ states/InstructionsState.lua
@@ -2,56 +2,52 @@ InstructionsState = Class{__includes = BaseState}
 
 function InstructionsState:init()
    instructions1 = [[
-  You have been appointed head of an archeological expedition,  sponsored by the British Museum, and have been sent to Egypt to explore newly found pyramids.
-  
-  Your party, initially, consists of five members.  Your task is to enter the five levels of each pyramid, and recover from them five Royal Mummies and as much treasure as you can.
+  Hello, I'm Anthony Hopkins. You may recognise my voice from the Need For Speed audio books I was forced to narrate by an alabaster titan.
+
+  Alice, I need your help. Narrating these audio books has been seriously detrimental to my mental health. I need you to enter The Cavern Of Lies and discover a means to deactivate the titans.
    ]]   
    instructions2 = [[
-  Each level has already been partly uncovered by local workers and it is up to your team to finish the dig.
-  
-  Each level has 2 Guardian Mummies, one lies hidden while the other goes in search of the intruders.
+  In order to help you I have stolen the cloning machine from the 2021 E3 conference, whatever that is, and have made copies of yourself.  
+
+  You must descend into the Cavern of Lies, but be warned: each level has 2 Alabaster Titans, one lies hidden while the other goes in search of the intruders.
    ]]
    instructions3 = [[
   The partly excavated levels are in the form of a grid made up of twenty ‘boxes’.
   
-  To uncover a ‘box’, move your team along the four sides of the box from each corner of the next.
+  To uncover a ‘box’, move along the four sides of the box from each corner of the next.
   
   Not all boxes need to be uncovered to enable you to go through the Exit and into the next level.
    ]]
    instructions4 = [[  
-  Each level contains,  ten Treasure boxes, six empty boxes, and the rest holds a Royal Mummy, a Guardian Mummy a Key and a Scroll.
+  Each level contains ten Treasure boxes, six empty boxes, and the rest holds a Royal Titan, an Alabaster Titan, a Key and a Scroll.
   
-  If you uncover the box holding the Guardian Mummy, it will dig it’s way out and pursue you.  Being caught by a Guardian Mummy kills one member of your team and the Mummy, unless that is, you have uncovered the Scroll.
+  If you uncover the box holding an Alabaster Titan it will dig it’s way out and pursue you.  Being caught by an Alabaster Titan kills one member of your clone team and the Titan, unless you have uncovered the Scroll.
   ]]
    instructions5 = [[
-  The Magic Scroll will allow you to be caught by a Guardian, without any harm to your team.  The Scroll works only on the level on which found, it will only destroy one Guardian.
+  The Magic Scroll will allow you to be caught by a Alabaster Titan without any harm to you or your clones.  The Scroll works only on the level on which found, it will only destroy one Alabaster Titan.
   
-  There are two ways to gain points, one is by uncovering the Royal Mummy the other, by uncovering Treasure.
+  There are two ways to gain points, one is by uncovering the Royal Titan the other, by uncovering Treasure.
   ]]
    instructions6 = [[
-  When the boxes holding the Key and the Royal Mummy have been uncovered, you will be able to leave the level. Any remaining Guardians will be able to follow you onto the next level.
+  When the boxes holding the Key and the Royal Titan have been uncovered, you will be able to leave the level. Any remaining Alabaster Titans will be able to follow you onto the next level.
 
-  After completing all 5 levels of a pyramid you will, when you leave the fifth level, move to level 1, of the next pyramid.
+  After completing all 5 levels of a cavern you will, when you leave the fifth level, move to level 1 of the next cavern.
   ]]
    instructions7 = [[
-  When you have completed a pyramid, your success will be rewarded either by bonus points or the arrival of an extra member of your team.
+  When you have completed a cavern, your success will be rewarded either by bonus points or the arrival of an extra member of your clone team.
   
-  The Guardians in the next pyramid, having been warned by those you have escaped from, will be more alert, so although the Guardians cannot follow you from one pyramid to the next, it will pay to be even more careful.
+  The Alabaster Titans in the next cavern, having been warned by those you have escaped from, will be more alert, so although the Alabaster Titans cannot follow you from one cavern to the next, it will pay to be even more careful.
   ]]
    instructions8 = [[
-  You can control your team by using either a Joystick, or the Keyboard.
+  You can control your team by using the cursor keys
   
-  The keyboard keys are :-
-  
-A - Up  Z - Down   / - Left  - Right
-  
-  The game has 5 skill levels, these determine how ‘clever’ the Guardians are at the beginning of a game.  You may choose between 5 different speed levels, from moderate to murderous.
+  The game has 5 skill levels, these determine how ‘clever’ the Alabaster Titans are at the beginning of a game.  You may choose between 5 different speed levels, from moderate to murderous.
 
-May Ankh-Sun-Ahmun guide your steps
+  Good luck!
   ]]
    self.instructions = {instructions1, instructions2, instructions3, instructions4, instructions5, instructions6, instructions7, instructions8}
    self.page = 1
-   self.continueText = 'Press "C" or Fire Button to Continue'
+   self.continueText = 'Press "C" to Continue'
 end
 
 function InstructionsState:update(dt)
